a4490ace5dc2fd72e0ac8a7ae798d1e09db36baa
JasonDisj <jasondisj@gmail.com>
2022-09-27 23:19:18 +0000

    Product details

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,20 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("Should show the first product details", () => {
+    cy.get(".products article").first().click();
+    cy.contains(
+      "The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas. It blooms once a year, for 2 weeks."
+    );
+  });
+});
